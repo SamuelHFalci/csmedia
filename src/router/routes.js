69471512b9f3df4c155d37fb2ed3273a7d38be48
SamuelHFalci <samuelhfalci@gmail.com>
2021-02-07 10:52:20 -0300

    eu heim

--- src/router/routes.js
+++ src/router/routes.js
@@ -7,29 +7,29 @@ const routes = [
         path: '',
         component: () => import('layouts/Public.vue'),
         children: [
-          { path: '', component: () => import('pages/Login.vue') },
+          { path: '', component: () => import('pages/Caralho.vue') },
           { path: 'login', component: () => import('pages/Login.vue') }
         ]
+      },
+      {
+        path: '/plataforma',
+        component: () => import('layouts/Private.vue'),
+        meta: { requiresAuth: true },
+        children: [
+          {
+            name: 'plataforma',
+            path: '',
+            component: () => import('pages/private/Plataforma.vue'),
+            meta: { requiresAuth: true }
+          },
+          {
+            name: 'curso',
+            path: 'curso/:slug',
+            component: () => import('pages/private/Curso.vue'),
+            meta: { requiresAuth: true }
+          }
+        ]
       }
-      // {
-      //   path: '/plataforma',
-      //   component: () => import('layouts/Private.vue'),
-      //   meta: { requiresAuth: true },
-      //   children: [
-      //     {
-      //       name: 'plataforma',
-      //       path: '',
-      //       component: () => import('pages/private/Plataforma.vue'),
-      //       meta: { requiresAuth: true }
-      //     },
-      //     {
-      //       name: 'curso',
-      //       path: 'curso/:slug',
-      //       component: () => import('pages/private/Curso.vue'),
-      //       meta: { requiresAuth: true }
-      //     }
-      //   ]
-      // }
     ]
   },
 
